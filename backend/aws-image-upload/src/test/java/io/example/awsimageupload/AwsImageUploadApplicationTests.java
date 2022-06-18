2a68af4e8a65f03b8bdf7cb6d2667760c0782743
Cody McCarty <codyjmccarty@gmail.com>
2022-06-18 19:41:26 -0400

    initial commit

---
+++ backend/aws-image-upload/src/test/java/io/example/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
